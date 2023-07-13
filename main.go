bb9be4a0fb4fbc894b6038ba4f43a58e5b7810b9
nutax <jose.huby@utec.edu.pe>
2023-07-12 22:12:35 -0500

    owo

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -10,28 +12,44 @@ import (
 )
 
 func main() {
+	fmt.Println("Hello, playground")
 	a := app.New()
 	w := a.NewWindow("Images")
 	bounds := screenshot.GetDisplayBounds(0)
-	img, err := screenshot.CaptureRect(bounds)
-	// Print the image Pix length
-	fmt.Println(len(img.Pix))
-	fmt.Println(img.Bounds())
-	fmt.Println(img.Rect.Max.X * img.Rect.Max.Y * 4)
-	// Print stride
-	fmt.Println(img.Stride)
-	fmt.Println(img.Rect.Max.X * 4)
-	// Iterate over the image Pix and change RGB values to the average of the three
-	for i := 0; i < len(img.Pix); i += 4 {
-		avg := (img.Pix[i] + img.Pix[i+1] + img.Pix[i+2]) / 3
-		img.Pix[i] = avg
-		img.Pix[i+1] = avg
-		img.Pix[i+2] = avg
-		img.Pix[i+3] = 255
+	m := make([]uint32, bounds.Dx()*bounds.Dy()*4)
+
+	for i := 0; i < 100; i++ {
+		img, err := screenshot.CaptureRect(bounds)
+
+		if err != nil {
+			panic(err)
+		}
+		img_size := len(img.Pix)
+		for i := 0; i < img_size; i += 4 {
+			m[i/4] += (uint32(img.Pix[i]) + uint32(img.Pix[i+1]) + uint32(img.Pix[i+2])) / 3
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
+	for i := 0; i < len(m); i++ {
+		m[i] /= 100
+	}
+
+	img, err := screenshot.CaptureRect(bounds)
 	if err != nil {
 		panic(err)
 	}
+
+	threshold := uint32(50)
+	for i := 0; i < len(img.Pix); i += 4 {
+		avg := (uint32(img.Pix[i]) + uint32(img.Pix[i+1]) + uint32(img.Pix[i+2])) / 3
+		diff := uint32(math.Abs(float64(avg) - float64(m[i/4])))
+		if diff > threshold {
+			img.Pix[i] = 0
+			img.Pix[i+1] = 0
+			img.Pix[i+2] = 0
+		}
+	}
+
 	img_img := img.SubImage(img.Rect)
 	img_canvas := canvas.NewImageFromImage(img_img)
 	w.SetContent(img_canvas)
